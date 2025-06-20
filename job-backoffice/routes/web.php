151085f898162b985db5daace1e4d66785c396fb
sfsf-mj <sfsfmj24.5@gmail.com>
2025-06-20 08:58:07 +0300

    Implemented all controllers and their routes for the application

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class,'index'])->name('dashboard');
+
+    Route::resource('company', CompanyController::class);
+    Route::resource('application', JobApplicationController::class);
+    Route::resource('category', JobCategoryController::class);
+    Route::resource('job-vacancy', JobVacancyController::class);
+    Route::resource('user', UserController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
